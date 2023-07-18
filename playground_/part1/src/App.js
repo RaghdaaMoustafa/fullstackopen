feae16321e80f24863bd7010c2c10fa106171b19
Raghdaa <raghda.mustafa.anwar@gmail.com>
2023-07-18 17:34:39 +0300

    -part1a -reading -components -props

---
+++ playground_/part1/src/App.js
@@ -0,0 +1,39 @@
+const Hello = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <p>
+        Hello {props.name}, you are {props.age} years old
+      </p>
+    </div>
+  )
+}
+const Footer = () => {
+  return (
+    <div>
+      greeting app created by <a href="https://github.com/mluukkai">mluukkai</a>
+    </div>
+  )
+}
+
+const App = () => {
+  const friends = [
+    { name: 'Peter', age: 4 },
+    { name: 'Maya', age: 10 },
+
+  ]
+  const fri=['meqdad','raghad']
+
+  return (
+    <div>
+      <p>{friends[0].name} {friends[0].age}</p>
+      <p>{friends[1].name} {friends[1].age}</p>
+      <p>{fri}</p>
+      <Hello name ='raghad'/>
+    </div>
+  )
+}
+
+
+
+export default App
