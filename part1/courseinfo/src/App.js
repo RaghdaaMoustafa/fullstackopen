13ea8ae056170291cdf646f7a45350e89572a510
Raghdaa <raghda.mustafa.anwar@gmail.com>
2023-07-18 18:23:48 +0300

    part1a -1.1 refactoring -decoupling

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,3 +1,20 @@
+const Header = (props) => {
+  return <h1>{props.name}</h1>
+}
+
+const Content = (props) => {
+  return (
+    <p>
+      The part name is {props.name} and number of excersices is
+      {props.excercise}
+    </p>
+  )
+}
+
+const Total = (props) => {
+  return <p>The total number of excersices is {props.number}</p>
+}
+
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -9,17 +26,11 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header name={course} />
+      <Content name={part1} excercise={exercises1} />
+      <Content name={part2} excercise={exercises2} />
+      <Content name={part3} excercise={exercises3} />
+      <Total number={exercises1 + exercises2 + exercises3} />
     </div>
   )
 }
