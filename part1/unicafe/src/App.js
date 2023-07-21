e0c5780d70f5108ab89144acccfc5648aa38834d
Raghdaa <raghda.mustafa.anwar@gmail.com>
2023-07-21 15:44:21 +0300

    -part1d -1.9 reduce on object, if condition

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,26 +1,39 @@
 import { useState } from 'react'
 
-const Display = ({ category, text }) => (
-  <div>
-    {text} {category}
-  </div>
-)
+const Display = ({ category, text }) => {
+  return (
+    <div>
+      {text} {category}
+    </div>
+  )
+}
+const Statistics = (props) => {
+  if (props.good === 0 && props.bad === 0 && props.neutral === 0) {
+    return <div>No feedback given</div>
+  }
+  return (
+    // const statistics=props.items.map((element)=>{<StatEntry category={element} text=})
+    <>
+      <Display category={props.good} text="good " />
+      <Display category={props.neutral} text="neutral " />
+      <Display category={props.bad} text="bad " />
+      <Display category={props.total} text="all " />
+      <Display category={props.average} text="average " />
+      <Display category={props.positive} text="positive " />
+    </>
+  )
+}
 const Button = ({ handleClick, text }) => (
   <button onClick={handleClick}>{text}</button>
 )
-// const Total = ({ props }) => {
-//   var total = props.values.reduce((sum, element) => {
-//     return (sum += element)
-//   }, 0)
-//   return <Display handleClick={total} text="all" />
-// }
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  const total = good + neutral + bad
+  // const total = good + neutral + bad
   const average = (a, b, c) => {
     return (a + b + c) / 3
   }
@@ -35,16 +48,23 @@ const App = () => {
       <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
       <Button handleClick={() => setBad(bad + 1)} text="bad" />
       <h1>statistics</h1>
-      <Display category={good} text="good " />
-      <Display category={neutral} text="neutral " />
-      <Display category={bad} text="bad " />
-      <Display category={total} text="all " />
-      <Display category={average(good, neutral, bad)} text="average " />
-      <Display category={positive(good, neutral, total)} text="positive " />
-
-      {/* Total item1={good} item2={neutral} item3={bad}  */}
+      <Statistics
+        good={good}
+        neutral={neutral}
+        bad={bad}
+        total={<Total items={{ good, neutral, bad }} />}
+        average={average(good, neutral, bad)}
+        positive={positive(good, neutral, good + neutral + bad)}
+      />
     </div>
   )
 }
 
 export default App
+
+const Total = ({ items }) => {
+  return Object.values(items).reduce((sum, item) => {
+    return (sum += item)
+  }, 0)
+  // console.log(total)
+}
