022e45e205b64e209d9f251b11ba55c90cde9bc6
Raghdaa <raghda.mustafa.anwar@gmail.com>
2023-07-22 19:00:29 +0300

    -part2a -2.5 seperate Course component file

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,45 +1,4 @@
-const Course = ({ courses }) => {
-  const Header = (props) => {
-    return <h2>{props.name}</h2>
-  }
-
-  const Part = (props) => {
-    return (
-      <>
-        <p>
-          The part name is {props.name} and number of excersices is{' '}
-          {props.excercise}
-        </p>
-      </>
-    )
-  }
-
-  const Content = (props) => {
-    return (
-      <>
-        {props.parts.map((item, i) => {
-          return <Part key={i} name={item.name} excercise={item.exercises} />
-        })}
-      </>
-    )
-  }
-
-  const Total = (props) => {
-    const summation = props.parts.reduce((sum, element) => {
-      return (sum += element.exercises)
-    }, 0)
-    return <p>The total number of excersices is {summation} </p>
-  }
-  return courses.map((element, i) => (
-    <div key={i}>
-      <Header name={element.name} />
-      <Content parts={element.parts} />
-      <b>
-        <Total parts={element.parts} />
-      </b>
-    </div>
-  ))
-}
+import Course from './component/Course'
 
 const App = () => {
   const courses = [
