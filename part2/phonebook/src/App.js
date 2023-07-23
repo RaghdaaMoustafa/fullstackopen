1841073bd46d2eb16c8e069b71d351f8bade6102
Raghdaa <raghda.mustafa.anwar@gmail.com>
2023-07-23 22:06:00 +0300

    -part2b -2.8 phonebook logic error alert() includes()

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,13 +3,22 @@ import { useState } from 'react'
 const App = () => {
   const [persons, setPersons] = useState([{ name: 'Arto Hellas' }])
   const [newName, setNewName] = useState('')
+  const checkRepeated = persons.map((element) => element.name === newName)
   const addName = (event) => {
     event.preventDefault()
     const phoneObject = {
       name: newName,
     }
+    console.log(persons)
+    console.log(checkRepeated)
+    console.log(checkRepeated.includes(true))
+
+    if (checkRepeated.includes(true)) {
+      alert(`${newName} is already added to phonebook`)
+    }
     setPersons(persons.concat(phoneObject))
     setNewName(' ')
+    console.log(persons)
   }
   const handleInput = (event) => {
     setNewName(event.target.value)
@@ -20,7 +29,7 @@ const App = () => {
       <h2>Phonebook</h2>
       <form onSubmit={addName}>
         <div>
-          name: <input value={newName} onChange={handleInput} />
+          name: <input onChange={handleInput} value={newName} />
         </div>
         <div>
           <button type="submit">add</button>
@@ -28,7 +37,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map((element) => (
+        {persons.map((element, id) => (
           <li key={element.name}>{element.name}</li>
         ))}
       </ul>
