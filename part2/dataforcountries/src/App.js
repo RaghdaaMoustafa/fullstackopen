fbf6d06a09d7bc5cca1fd53ad7426ebc7c05be27
Raghdaa <raghda.mustafa.anwar@gmail.com>
2023-07-28 05:18:02 +0300

    -part2e -2.18 create react app

--- part2/dataforcountries/src/App.js
+++ part2/dataforcountries/src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { useState, useEffect } from 'react'
 import Filter from './Components/Filter'
 import Display from './Components/Country'
@@ -33,3 +34,30 @@ const App = () => {
   )
 }
 export default App
+=======
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+>>>>>>> 150391e (-part2e -2.18 create react app)
