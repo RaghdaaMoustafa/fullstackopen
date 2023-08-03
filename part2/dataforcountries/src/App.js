0c9435d9f564c7340ff1b83dce0452eed11b8dee
Raghdaa <raghda.mustafa.anwar@gmail.com>
2023-08-04 00:59:49 +0300

    -merge

--- part2/dataforcountries/src/App.js
+++ part2/dataforcountries/src/App.js
@@ -1,40 +1,3 @@
-<<<<<<< HEAD
-import { useState, useEffect } from 'react'
-import Filter from './Components/Filter'
-import Display from './Components/Country'
-import countriesService from './Services/countries'
-const App = () => {
-  const [countries, setCountries] = useState([])
-  const [filtered, setFiltered] = useState('')
-  
-  useEffect(() => {
-    countriesService.getAll().then((response) => {
-      setCountries(response)
-    })
-  }, [])
-
-  const countriesToShow = countries.filter((element) =>
-    element.name.common.toLowerCase().includes(filtered.toLocaleLowerCase())
-  )
-  
-  const handleFilter = (event) => {
-    const newValue = event.target.value
-    setFiltered(newValue)
-  }
-
-  return (
-    <div>
-      <Filter value={filtered} handleFilter={handleFilter} />
-
-      <Display
-        countriesToShow={countriesToShow}
-        filter={filtered}
-      />
-    </div>
-  )
-}
-export default App
-=======
 import logo from './logo.svg';
 import './App.css';
 
@@ -60,4 +23,3 @@ function App() {
 }
 
 export default App;
->>>>>>> 150391e (-part2e -2.18 create react app)
