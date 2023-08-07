0db4f3ef7cf9b7a6f49035ebe536b1f6bd4a3a09
Raghdaa <raghda.mustafa.anwar@gmail.com>
2023-08-07 05:08:00 +0300

    -part3c -3.12 mongoose mongodb Atlas save data find data process.argv[3]

---
+++ part3/phonebook-backend/mongo.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+const name = process.argv[3]
+const number = process.argv[4]
+
+const url = `mongodb+srv://raghdamustafa:${password}@cluster0.bintyzv.mongodb.net/phonebookApp?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+
+const phonebookSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', phonebookSchema)
+
+const person = new Person({
+  name: `${name}`,
+  number: `${number}`,
+})
+
+if (process.argv.length < 4) {
+  Person.find({}).then((result) => {
+    result.forEach((person) => console.log(person))
+    mongoose.connection.close()
+  })
+} else {
+  person.save().then((result) => {
+    console.log(`added ${name} number ${number} to phonebook`)
+    mongoose.connection.close()
+  })
+}
+// Note.find({important: true}).then(result => {
+//     result.forEach(note => {
+//       console.log(note)
+//     })
+//     mongoose.connection.close()
+//   })
