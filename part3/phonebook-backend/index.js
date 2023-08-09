25fa5a93dd59abb3b19e2c6e9c78c3839e25a768
raghad moustafa <raghda.mustafa.anwar@gmail.com>
2023-08-10 02:03:17 +0300

    -part3c  -3.13 fetch from database

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,4 +1,6 @@
 const express = require('express')
+require('dotenv').config()
+const Person = require('./models/phone')
 const morgan = require('morgan')
 const cors = require('cors')
 const app = express()
@@ -12,28 +14,6 @@ morgan.token('body', (req) => {
 
 app.use(morgan(':method :url :body'))
 
-let phones = [
-  {
-    id: 1,
-    name: 'Arto Hellas',
-    number: '040-123456',
-  },
-  {
-    id: 2,
-    name: 'Ada Lovelace',
-    number: '39-44-5323523',
-  },
-  {
-    id: 3,
-    name: 'Dan Abramov',
-    number: '12-43-234345',
-  },
-  {
-    id: 4,
-    name: 'Mary Poppendieck',
-    number: '39-23-6423122',
-  },
-]
 const time = new Date()
 const generateid = () => {
   const maxId = Math.max(...phones.map((person) => person.id))
@@ -41,24 +21,28 @@ const generateid = () => {
 }
 
 app.get('/api/persons', (request, response) => {
-  response.json(phones)
+  Person.find({}).then((person) => {
+    response.json(person)
+  })
 })
 app.get('/info', (request, response) => {
-  response.send(
-    `<div><p>Phonebook has info ${
-      phones.length
-    } people</p> <p>${new Date()}</p></div>`
-  )
+  Person.find({}).then((phones) => {
+    response.json(
+      `<div><p>Phonebook has info ${
+        phones.length
+      } people</p> <p>${new Date()}</p></div>`
+    )
+  })
 })
 app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const person = phones.find((person) => person.id === id)
-  if (person) {
-    response.json(person)
-  } else {
-    response.statusMessage = 'No such id'
-    response.status(404).end()
-  }
+  Person.findById(request.params.id)
+    .then((phone) => {
+      response.json(phone)
+    })
+    .catch((error) => {
+      response.statusMessage = 'No such id'
+      response.status(404).end()
+    })
 })
 app.delete('/api/persons/:id', (request, response) => {
   const id = Number(request.params.id)
