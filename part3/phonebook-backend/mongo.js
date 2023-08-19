31c3eecff00d95550fae24298fa1991aec4c47b4
raghad moustafa <raghda.mustafa.anwar@gmail.com>
2023-08-19 21:27:56 +0300

    -ponebook -eslint to mongo.js

--- part3/phonebook-backend/mongo.js
+++ part3/phonebook-backend/mongo.js
@@ -32,7 +32,7 @@ if (process.argv.length < 4) {
     mongoose.connection.close()
   })
 } else {
-  person.save().then((result) => {
+  person.save().then(() => {
     console.log(`added ${name} number ${number} to phonebook`)
     mongoose.connection.close()
   })
