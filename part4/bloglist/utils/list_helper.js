eb871ed79e6e3305393e05e7fbc86df1e1d5e9d1
raghad moustafa <raghda.mustafa.anwar@gmail.com>
2023-08-25 00:35:38 +0300

    -part4a -4.6* lodash library groupBy() maxBy() Object.keys().map()

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,3 +1,4 @@
+const _ = require('lodash')
 const dummy = (blogs) => {
   return 1
 }
@@ -12,4 +13,47 @@ const favoriteBlog = (blogs) => {
   const max = blogs.reduce((max, element) => Math.max(max, element.likes), 0)
   return blogs.length === 0 ? 0 : blogs.find((element) => element.likes === max)
 }
-module.exports = { dummy, totalLikes, favoriteBlog }
+// const mostBlogs = (blogs) => {
+//   if (blogs.length === 0) {
+//     return 0
+//   }
+//   let authorMap = {}
+//   let maxAuthor = blogs[0].author
+//   let maxCount = 1
+//   for (let i = 0; i < blogs.length; i++) {
+//     let element = blogs[i].author
+//     console.log(element)
+//     if (authorMap[element] === null) {
+//       authorMap[element] = 1
+//     } else {
+//       authorMap[element] += 1
+//     }
+//     if (authorMap[element] > maxCount) {
+//       maxAuthor = element
+//       maxCount = authorMap[element]
+//       console.log(maxAuthor)
+//       console.log(maxCount)
+//     }
+//   }
+//   console.log(authorMap)
+//   const maxBlog = {
+//     author: maxAuthor,
+//     blogs: maxCount,
+//   }
+//   return maxBlog
+// }
+
+const mostBlogs = (blogs) => {
+  if (blogs.length === 0) {
+    return 0
+  }
+  const grouped = _.groupBy(blogs, (blog) => blog.author)
+
+  const blogSum = Object.keys(grouped).map((items) => {
+    objectMax = { author: items, blogs: grouped[items].length }
+    return objectMax
+  })
+  return _.maxBy(blogSum, 'blogs')
+}
+
+module.exports = { dummy, totalLikes, favoriteBlog, mostBlogs }
