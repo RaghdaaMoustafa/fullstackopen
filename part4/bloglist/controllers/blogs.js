7d34822866eda90de15ca2c46e601e5837067b41
raghad moustafa <raghda.mustafa.anwar@gmail.com>
2023-09-01 02:51:31 +0300

    -part4b -4.11* change schema and make a default value to an attribute solve the time out problem in the beforeEach fn

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -8,16 +8,16 @@ blogRouter.get('/', (request, response) => {
 })
 
 blogRouter.post('/', async (request, response) => {
-  //   const payload = request.body
-  //   const blog = new Blog({
-  //     title: payload.title,
-  //     author: payload.author,
-  //     url: payload.url,
-  //     likes: payload.likes,
-  //   })
-  //   console.log('payload: ', payload)
-  //   console.log('blog: ', blog)
-  const blog = new Blog(request.body)
+  const payload = request.body
+  const blog = new Blog({
+    title: payload.title,
+    author: payload.author,
+    url: payload.url,
+    likes: payload.likes,
+  })
+  console.log('payload: ', payload)
+  console.log('blog: ', blog)
+  // const blog = new Blog(request.body)
   const savedBlog = await blog.save()
   response.status(201).json(savedBlog)
 })
--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -8,38 +8,32 @@ const Blog = require('../models/blog')
 
 const initialBlogs = [
   {
-    _id: '5a422a851b54a676234d17f7',
     title: 'React patterns',
     author: 'Michael Chan',
     url: 'https://reactpatterns.com/',
     likes: 7,
-    __v: 0,
   },
   {
-    _id: '5a422aa71b54a676234d17f8',
     title: 'Go To Statement Considered Harmful',
     author: 'Edsger W. Dijkstra',
     url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
     likes: 5,
-    __v: 0,
   },
   {
-    _id: '5a422b3a1b54a676234d17f9',
     title: 'Canonical string reduction',
     author: 'Edsger W. Dijkstra',
     url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
     likes: 12,
-    __v: 0,
   },
 ]
 
 beforeEach(async () => {
   await Blog.deleteMany({})
-
+  // await Blog.insertMany(initialBlogs)
   const blogObjects = initialBlogs.map((blog) => new Blog(blog))
   const promiseArray = blogObjects.map((blog) => blog.save())
   await Promise.all(promiseArray)
-})
+}, 100000)
 test('blogs are returned as json', async () => {
   await api
     .get('/api/blogs')
@@ -60,12 +54,10 @@ test('verify that the unique identifier property of the blog posts is named id',
 })
 test('a new blog post can be made', async () => {
   const newBlog = {
-    _id: '5a422bc61b54a676234d17fc',
     title: 'Type wars',
     author: 'Robert C. Martin',
     url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
     likes: 2,
-    __v: 0,
   }
   console.log(newBlog)
   await api
@@ -77,6 +69,21 @@ test('a new blog post can be made', async () => {
   const response = await api.get('/api/Blogs')
   console.log(response.body)
   expect(response.body).toHaveLength(initialBlogs.length + 1)
+})
+test('default value of like property to 0', async () => {
+  const newBlog = {
+    title: 'raghad me',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
+  }
+  console.log(newBlog)
+  const response = await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  expect(response.body.likes).toBe(0)
 })
 afterAll(async () => {
   await mongoose.connection.close()
