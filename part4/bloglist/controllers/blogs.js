24b5d5158e117f7f759b570a2596e6ad00f866f9
raghad moustafa <raghda.mustafa.anwar@gmail.com>
2023-09-09 00:06:43 +0300

    -part4d -4.17 -authorize -populate

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,25 +1,31 @@
 const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
-blogRouter.get('/', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
+blogRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
+  response.json(blogs)
 })
 
 blogRouter.post('/', async (request, response) => {
   const payload = request.body
+  console.log(payload)
+  const user = await User.findById(payload.userId)
+  console.log(user)
   const blog = new Blog({
     title: payload.title,
     author: payload.author,
     url: payload.url,
     likes: payload.likes,
-    
+    user: user.id,
   })
   // console.log('payload: ', payload)
   // console.log('blog: ', blog)
   // const blog = new Blog(request.body)
   const savedBlog = await blog.save()
+  console.log(savedBlog)
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
   response.status(201).json(savedBlog)
 })
 blogRouter.delete('/:id', async (request, response) => {
--- part4/bloglist/models/user.js
+++ part4/bloglist/models/user.js
@@ -4,6 +4,12 @@ const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true, minlength: 3 },
   name: String,
   passwordHash: { type: String, required: true, minlength: 3 },
+  blogs: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Blog',
+    },
+  ],
 })
 userSchema.plugin(uniqueValidator)
 userSchema.set('toJSON', {
