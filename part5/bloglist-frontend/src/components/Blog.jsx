95259f785ef1cca58e1d8d862fb3d0f350251461
raghad moustafa <raghda.mustafa.anwar@gmail.com>
2023-10-01 21:12:10 +0300

    -part5b -5.7 button for showing details

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,41 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(false)
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+  }
+  return (
+    <>
+      <div style={blogStyle}>
+        <button
+          onClick={() => {
+            setShowDetails(!showDetails)
+          }}
+        >
+          {showDetails ? 'hide' : 'view'}
+        </button>
+        {showDetails ? (
+          <>
+            <div>{blog.title}</div>
+            <div>{blog.url}</div>
+            <div>
+              {blog.likes}
+              <button>like</button>
+            </div>
+            <div>{blog.author}</div>
+          </>
+        ) : (
+          <div>
+            {blog.title} {blog.author}
+          </div>
+        )}
+      </div>
+    </>
+  )
+}
 
 export default Blog
